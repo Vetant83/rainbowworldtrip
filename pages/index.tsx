9390702f262197905ff0b98236880007e6bcb6e6
Vetant83 <sharmavetant@gmail.com>
2026-01-17 16:29:10 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Rainbow World Trip</h1>
+      <p>Website is successfully deployed.</p>
+    </main>
+  );
+}
 
